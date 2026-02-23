2ba64c03e2dfa6770fdae2ed2b5f7caad3cfbf03
Oleksii <olexijukraina@gmail.com>
2026-02-23 17:08:20 +0200

    Add style improvements and add DOMContentLoaded

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,35 +1,38 @@
 'use strict';
 
-const logo = document.querySelector('.logo');
+document.addEventListener('DOMContentLoaded', () => {
+  const logo = document.querySelector('.logo');
 
-const showSuccess = () => {
-  const div = document.createElement('div');
+  const showSuccess = () => {
+    const div = document.createElement('div');
 
-  div.className = 'message';
-  div.textContent = 'Promise was resolved!';
-  document.body.appendChild(div);
-};
+    div.classList.add('message');
+    div.textContent = 'Promise was resolved!';
+    document.body.appendChild(div);
+  };
 
-const showError = () => {
-  const div = document.createElement('div');
+  const showError = () => {
+    const div = document.createElement('div');
 
-  div.className = 'message error-message';
-  div.textContent = 'Promise was rejected!';
-  document.body.appendChild(div);
-};
+    div.classList.add('message', 'error-message');
+    div.textContent = 'Promise was rejected!';
+    document.body.appendChild(div);
+  };
 
-const promise1 = new Promise((resolve) => {
-  logo.addEventListener('click', () => {
-    resolve();
+  const promise1 = new Promise((resolve) => {
+    logo.addEventListener('click', () => {
+      resolve();
+    });
   });
+
+  const promise2 = new Promise((resolve, reject) => {
+    void resolve;
+
+    setTimeout(() => {
+      reject(new Error('Promise was rejected'));
+    }, 3000);
+  });
+
+  promise1.then(showSuccess).catch(showError);
+  promise2.then(showSuccess).catch(showError);
 });
-
-promise1.then(showSuccess).catch(showError);
-
-const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject(new Error('Promise was rejected'));
-  }, 3000);
-});
-
-promise2.then(showSuccess).catch(showError);
